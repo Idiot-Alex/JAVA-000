8a1046db69b91bd595e31081c7b9a7faade74aa6
Eleven <idiotalex@163.com>
2020-12-02 23:09:14 +0800

    完成作业 2 学习笔记

---
+++ Week_07/src/main/java/com/hotstrip/week7_work2/Week7Work2Application.java
@@ -0,0 +1,13 @@
+package com.hotstrip.week7_work2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7Work2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week7Work2Application.class, args);
+	}
+
+}
---
+++ Week_07/src/test/java/com/hotstrip/week7_work2/Week7Work2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotstrip.week7_work2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7Work2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
